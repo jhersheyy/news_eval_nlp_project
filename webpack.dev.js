2f9d955ed7d7325f9d4535d37be4bdd1baae351d
Julia Hershey <jhershey@Julias-MacBook-Pro.local>
2020-07-25 22:48:45 -0700

    added loaders to convert sass to css

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,6 +14,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/, // NOTE:NO QUOTES
+                use: [ 'style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
--- webpack.prod.js
+++ webpack.prod.js
@@ -11,6 +11,10 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/, // NOTE:NO QUOTES
+                use: [ 'style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
