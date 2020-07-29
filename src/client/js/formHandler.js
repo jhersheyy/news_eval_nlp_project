179348a200bf3473e9300809e483a2cfd7d5ab75
Julia Hershey <jhershey@Julias-MacBook-Pro.local>
2020-07-28 23:13:14 -0700

    weather api now works on both prod and dev mode

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,12 @@
 async function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
+    // check what text was put into the form field:
     let formText = document.getElementById('zip').value
     Client.checkForName(formText)
-    
-    let baseUrl = 'https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?zip='
-    //+formText
-    let medQuery = '&units=imperial&appid=';
 
     console.log("::: Form Submitted :::")
-
+    //send url to server side thru post endpt:
     const response = await fetch('http://localhost:8081/testpost', {
         method: 'POST', 
         credentials: 'same-origin',
@@ -19,24 +15,23 @@ async function handleSubmit(event) {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({url: formText}), 
-      })//send url to server side
+      })
+      //relay data from server side:
     .then(res => {
-        let post_res = res.text();
-        //console.log("back from the server side: ", post_res);
+        let post_res = res.json();
+        console.log("back from the server side: ", post_res);
         return post_res
-        //const testget = fetch('http://localhost:8081/testget', {
-          //  method: 'GET',
-            //mode: 'same-origin',
-            //headers: {
-            //    'Content-Type': 'application/json',
-            //},
-            //body: JSON.stringify(data),
     })
-    .then(function(data) { //data is {url: "zip"}
-        console.log("DATUR: ", data)
-        console.log(data, "vs", data.main, "vs", data.main.temp)
-        document.getElementById('results').innerHTML = data.main.temp
-    })
+    //handle promise error 
+    .catch((error) => {
+        console.log("error:: ", error);
+    });
+
+    //check results:
+    console.log("DATUR: ", response);
+    console.log("DATA.MAIN: ",response.main);
+    console.log("DATA MAIN TEMP: ", response.main.temp);
+    document.getElementById('results').innerHTML = response.main.temp;  
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -4,11 +4,14 @@ var path = require('path')
 const express = require('express')
 var bodyParser = require('body-parser') // to use url encoded values
 var cors = require('cors') // to use json
+const fetch = require('node-fetch');
+
 let input_url = [];
 //const mockAPIResponse = require('./mockAPI.js')
 const app = express()
 
-const baseURL = 'https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?zip=';
+//NOT WORKING: const baseURL = 'https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?zip=';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 const key= process.env.APP_ID;
 app.use(cors())
 
@@ -32,34 +35,32 @@ app.get('/', function (req, res) {
     //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
-app.get('/testget', function (req, res) {
+//app.get('/testget', function (req, res) {
     //res.send(mockAPIResponse)
-    const info =  fetch(baseURL+input_url+process.env.APP_ID)
+  //  const info =  fetch(baseURL+input_url+process.env.APP_ID)
 
-    res.send(info)
-})
-const getWeather = async (baseURL, input_url, key)=>{
-    const res = await fetch(baseURL+input_url+key)
-    try {
-      const data = await res.json();
-      return data;
-    }  catch(error) {
-      console.log("error", error);
-      // appropriately handle the error
-    }
-  }
+    //res.send(info)
+//})
+
 // Post Route
 app.post('/testpost', addPost);
-//let input_url = [];
-function addPost(req,res){
+async function addPost(req,res){
+    //get and set url:
     newEntry = req.body
-    console.log("REQ IS DIS: ",req.body)
+    console.log("REQ BODY IS DIS: ",req.body)
     input_url = req.body.url;
     console.log("input_url now set to: ", input_url)
-//  projectData = newEntry;
-    const info =  getWeather(baseURL, input_url, key);//fetch(baseURL+input_url+process.env.APP_ID)
-
-    res.send(info);
+    
+    //get weather data from input:
+    const info = await fetch(baseURL+input_url+key)
+    //translate response obj to json:
+    .then( (info) => info.json())
+    //return json obj to client thru res.send():
+    .then( data => { console.log("TADATA!!!: ",data);res.send(data)})
+    //handle error:
+    .catch((error) => {
+        console.log("error:: ", error);
+    });
 };
 
 
